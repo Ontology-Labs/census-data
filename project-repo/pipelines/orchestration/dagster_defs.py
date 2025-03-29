89f600b976607a2bf46bcd19edb8f8c0d0e82143
= <jochanolm@gmail.com>
2025-03-28 22:23:26 -0700

    blerge

--- project-repo/pipelines/orchestration/dagster_defs.py
+++ project-repo/pipelines/orchestration/dagster_defs.py
@@ -8,7 +8,6 @@ from pipelines.scraping.warpcast.accounts.scrape import WarpcastAccountScraper
 from pipelines.scraping.warpcast.channels.assets import farcaster_channels_to_s3
 from pipelines.scraping.warpcast.channels.scrape import FarcasterChannelScraper
 # Import the Neo4j ingest asset
-from pipelines.ingestion.warpcast.accounts.assets import warpcast_accounts_to_neo4j
 
 @job
 def warpcast_s3_job():
@@ -20,21 +19,18 @@ def farcaster_channels_s3_job():
     """Job that loads Farcaster channel data into S3"""
     farcaster_channels_to_s3()
 
-@job
-def warpcast_neo4j_job():
-    """Job that loads Warpcast account data into Neo4j"""
-    warpcast_accounts_to_neo4j()
 
 # Single source of truth for all Dagster definitions
 defs = Definitions(
     assets=[
         warpcast_users_to_s3, 
         farcaster_channels_to_s3, 
-        warpcast_accounts_to_neo4j
     ],
     jobs=[
         warpcast_s3_job, 
         farcaster_channels_s3_job, 
-        warpcast_neo4j_job
     ],
 )
+
+
+
